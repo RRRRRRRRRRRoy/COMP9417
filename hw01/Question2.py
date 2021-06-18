633e7400b8c1dbeaa3842819b63c89dff95810d7
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-06-18 22:45:30 +1000

    updating q2 a

--- hw01/Question2.py
+++ hw01/Question2.py
@@ -1,10 +1,12 @@
 import numpy as np
 import seaborn as sbn
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Reading the data from the data.csv file
-csv_data = pd.read_csv("./data.csv")
+csv_data = pd.read_csv("./hw01/data.csv")
 # print the result of reading
 print(csv_data)
 
 sbn.pairplot(csv_data)
+plt.show()
