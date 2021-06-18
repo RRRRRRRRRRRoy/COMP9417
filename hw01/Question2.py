aaacf76b1598a8c0ef869fbe8242440f1e5de807
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-06-18 22:24:00 +1000

    Q2 reading data

---
+++ hw01/Question2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import seaborn as sbn
+import pandas as pd
+
+# Reading the data from the data.csv file
+csv_data = pd.read_csv("./data.csv")
+# print the result of reading
+print(csv_data)
