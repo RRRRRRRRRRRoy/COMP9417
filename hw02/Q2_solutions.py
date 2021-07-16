462dcd2716f4f66f6acc17ae6ab650d8d067b662
RRRRRRRRRRRoy <RRRRRRRRRRRoy720@gmail.com>
2021-07-16 09:57:08 -0700

    updating question2 b

--- hw02/Q2_solutions.py
+++ hw02/Q2_solutions.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 import jax.numpy as jnp
 from jax import grad, random
 import jax as jax
+from scipy.optimize import minimize
 import tqdm as tqdm
 #####################################################################
 # Question2 a
@@ -17,13 +18,13 @@ A = np.array([[1, 0, 1, -1], [-1, 1, 0, 2], [0, -1, -2, 1]])
 b = np.array([[1], [2], [3]])
 x_k = np.array([[1], [1], [1], [1]])
 learning_rate = 0.1
-A_T = A.transpose()
+
 result_dict = {0: x_k}
 result_list = [0]
 
 norm_list = list()
 for index in range(1, 9999):
-    current_gradients = np.dot(A_T, (np.dot(A, x_k)-b))
+    current_gradients = np.dot(A.T, (np.dot(A, x_k)-b))
     gradient_norm = np.linalg.norm(current_gradients)
     next_x = x_k-learning_rate*current_gradients
     if gradient_norm < 0.001:
@@ -39,8 +40,8 @@ for index in range(1, 9999):
         x_k = next_x
 
 # Delete i times --> Using norm list to select corrent answer in the result_dict
-for i in range(abs(len(result_dict) - len(norm_list))):
-    del result_dict[len(norm_list)-i]
+# for i in range(abs(len(result_dict) - len(norm_list))):
+#     del result_dict[len(norm_list)-i]
 
 print("Here is the answer of question2 (a):")
 for i in range(5):
@@ -57,58 +58,62 @@ print()
 #####################################################################
 # Question2 b
 # From this question we can define A and b in the first step
-# We can get the following α=((AA^T g(x))^T g(x)+〖((Ax^((k) ) )〗^T-b^T AA^T)g(x))/(〖2(AA^T g(x))〗^T AA^T g(x) )
-# g(x)=(Ax^((k) )-b)
+# We can get the following ((A∇f(x^((k) ) ))^T (Ax^((k) )-b)+((Ax^((k) )-b))^T A∇f(x^((k) ) ))/(〖2(A∇f(x^((k) ) ))〗^T A∇f(x^((k) ) ) )
 #####################################################################
 A = np.array([[1, 0, 1, -1], [-1, 1, 0, 2], [0, -1, -2, 1]])
 b = np.array([[1], [2], [3]])
 x_k_b = np.array([[1], [1], [1], [1]])
 
-A_T = A.T
 
 
-def learning_rate_b(x_k_b):
-    Top_line = np.dot((np.dot(np.dot(A,A.T),(np.dot(A,x_k_b)-b))).T,(np.dot(A,x_k_b)-b))+np.dot(((np.dot(A,x_k_b)).T-np.dot(np.dot(b.T,A),A.T)),(np.dot(A,x_k_b)-b))
-    Bottom_line = 2*np.dot(np.dot(np.dot((np.dot(np.dot(A,A.T),(np.dot(A,x_k_b)-b))).T,A),A.T),(np.dot(A,x_k_b)-b))
-    learning_rate_result = np.float32(Top_line/Bottom_line)
+def learning_rate_function(x_k_b):
+    A_x_k_b = np.dot(A,x_k_b)-b
+    A_f_x_k = np.dot(A,np.dot(A.T,A_x_k_b))
+    Top_line = np.dot((A_f_x_k.T),A_x_k_b) + np.dot((A_x_k_b.T),A_f_x_k)
+    Bottom_line = 2 * np.dot((A_f_x_k.T),A_f_x_k)
+    learning_rate_result = Top_line/Bottom_line
     return learning_rate_result
 
-learning_rate_init = learning_rate_b(x_k_b)
-# print(learning_rate_init)
 
-learning_rate_lst = {0:learning_rate_init}
-x_k_b_lst = {0:x_k_b}
+# # print(current_learning_rate)
+# # print(learning_rate_function(x_k_b))
 
+learning_rate_lst = [0.1]
+x_k_b_dict = {0:x_k_b}
+norm_checker = list()
 
-norm_list_b = list()
-for index in range(1, 200):
-    current_gradients_b = np.dot(A_T, (np.dot(A, x_k_b)-b))
+for index in range(9999):
+    current_gradients_b = np.dot(A.T, (np.dot(A, x_k_b)-b))
+    # print(f"gradients: {current_gradients_b}")
+    # print(f"learning rate: {current_learning_rate}")
     # print(current_gradients_b)
+    if index ==0:
+        current_learning_rate = 0.1
+    else:
+        current_learning_rate = learning_rate_function(x_k_b)
+
+    next_x_b = x_k_b - current_learning_rate * current_gradients_b
+    # print(f"The next x: {next_x_b}")
     gradient_norm_b = np.linalg.norm(current_gradients_b)
-    # print(gradient_norm_b)
-    learning_rate_nxt = learning_rate_b(x_k_b)
-    # print(learning_rate_nxt)
-    next_x_b = x_k_b-learning_rate_nxt*current_gradients_b
-    # print(next_x_b)
+    # print(f"norm: {gradient_norm_b}")
+    # print(f"the norm value: {gradient_norm_b}")
     if gradient_norm_b < 0.001:
         break
     else:
-        norm_list_b.append(gradient_norm_b)
-        x_k_b_lst.update({index: next_x_b})
-        learning_rate_lst.update({index: learning_rate_nxt})
-        # test the list
+        next_learning_rate = learning_rate_function(x_k_b)
+        # print(f"learning rate: {next_learning_rate}")
+        norm_checker.append(gradient_norm_b)
+        x_k_b_dict.update({index:next_x_b})
+        learning_rate_lst.append(next_learning_rate)
+        current_learning_rate = next_learning_rate
         x_k_b = next_x_b
 
-# print(norm_list_b)
-# print(x_k_b_lst)
-# print(learning_rate_lst)
 
-#####################################################################
-# Question2 c
-# Steepest is faster, value change
-#####################################################################
-
-
+# index_array_d = np.array([i for i in range(len(learning_rate_lst))])
+# ???
+plt.plot()
+plt.plot(index_array_d,learning_rate_lst)
+plt.show()
 #####################################################################
 # Question2 d
 #####################################################################
@@ -158,30 +163,30 @@ Test_Y = Total_Y[half_index:len(Total_Y)]
 • last row Y test: 63.9
 '''
 
-print("Here is the answer of question2 (d):")
-print(f"first row X train: {Train_X[0]}")
-print(f"last row X train: {Train_X[-1]}")
-print(f"first row X test: {Test_X[0]}")
-print(f"last row X test: {Test_X[-1]}")
-print(f"first row Y train: {Train_Y[0]}")
-print(f"last row Y train: {Train_Y[-1]}")
-print(f"first row Y test: {Test_Y[0]}")
-print(f"first row Y test: {Test_Y[-1]}")
-print()
+# print("Here is the answer of question2 (d):")
+# print(f"first row X train: {Train_X[0]}")
+# print(f"last row X train: {Train_X[-1]}")
+# print(f"first row X test: {Test_X[0]}")
+# print(f"last row X test: {Test_X[-1]}")
+# print(f"first row Y train: {Train_Y[0]}")
+# print(f"last row Y train: {Train_Y[-1]}")
+# print(f"first row Y test: {Test_Y[0]}")
+# print(f"first row Y test: {Test_Y[-1]}")
+# print()
 #####################################################################
 # Question2 e
 #####################################################################
 adding_one = np.array([[1] for i in range(len(Train_X))])
-print(adding_one.shape)
+# print(adding_one.shape)
 new_Train_X = np.hstack((adding_one, Train_X))
 # inputs = jnp.array(Train_X)
-print(new_Train_X.shape)
+# print(new_Train_X.shape)
 # print(new_Train_X)
 inputs = jnp.array(new_Train_X)
 targets = jnp.array(Train_Y)
 W = jnp.array([[1.0, 1.0, 1.0, 1.0]])
-print(W.shape)
-print(W.T.shape)
+# print(W.shape)
+# print(W.T.shape)
 
 def predict(W):
   para_w_T = W.T
@@ -196,10 +201,10 @@ def loss(W):
 
 predict_result = predict(W)
 loss_result = loss(W)
-print(loss_result)
+# print(loss_result)
 
 W_grad = grad(loss)(W)
-print(W_grad)
+# print(W_grad)
 
 learning_rate = 1
 
@@ -220,9 +225,6 @@ for index in range(99999):
 
 weight_array = jnp.array(weight_list)
 index_list = [i for i in range(len(loss_list))]
-plt.figure(figsize=(6, 6))
-plt.plot(index_list, loss_list)
-
 train_loss = loss(weight_array[-1])
 # print(train_loss)
 
@@ -241,15 +243,38 @@ e_test_x = jnp.array(new_test_x)
 e_test_y = jnp.array(Test_Y)
 test_loss = loss_test(weight_array[-1])
 
+w_final = weight_array[-1]
+# Using MAE to calculate the accuracy
+def accuracy(targets,preds):
+    return jnp.mean(jnp.abs(targets - preds))
+
+train_acc = accuracy(targets,predict(w_final))
+test_acc = accuracy(e_test_y, predict_test(w_final))
+
+
+print()
 print("Here is the answer of question2 (e):")
 print(f"Iterration: {len(loss_list)-1}")
 print(f"The final weight is: {weight_array[-1]}")
 print(f"The Train loss is: {train_loss}")
 print(f"The Test loss is: {test_loss}")
+print(f"The Train accuracy is: {train_acc}")
+print(f"The Test accuracy is: {test_acc}")
+
+
+plt.plot(index_list, loss_list)
+plt.show()
+print()
+
 # #####################################################################
 # # Question2 f
 # #####################################################################
+A_f = np.array([[1, 0, 1, -1], [-1, 1, 0, 2], [0, -1, -2, 1]])
+b_f = np.array([[1], [2], [3]])
+x_k_b_f = np.array([[1], [1], [1], [1]])
 
-# #####################################################################
-# # Question2 g
-# #####################################################################
+w0 = jnp.array([[1.0, 1.0, 1.0, 1.0]])
+optimal = minimize(learning_rate_function,x_k_b,method="BFGS")   
+w_optimize = optimal
+# print(w_optimize)
+
