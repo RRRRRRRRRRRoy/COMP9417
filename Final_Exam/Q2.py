0e99a74ae76761db1043348d27d8cca52d8b4665
RRRRRRRRRRRoy <rrrrrrrrrrroy720@gmail.com>
2021-08-19 23:25:45 +1000

    Question 2b getting training x and y

--- Final_Exam/Q2.py
+++ Final_Exam/Q2.py
@@ -9,11 +9,32 @@ data2 = ["011", "100", "110", "111"]
 data3 = ["0100", "0101", "0110", "1000", "1100", "1101", "1110", "1111"]
 data4 = ["1000000", "1000001", "1000101"]
 
-print(len(data1[0]))
-print(len(data2[0]))
-print(len(data3[0]))
-print(len(data4[0]))
-X = np.asarray(list(itertools.product([0, 1], repeat=len(data4[-1]))))
-y = -1 * np.ones(len(X))
-print(X)
-print(y)
+# For X in data4 which length is 7
+# Therefore, the total situation in dataset X which is 2^7=128.
+# This is because the current feature only has 0 or 1 which are 2 situations
+
+
+def train_perceptron(data):
+    # Generating all situation of 0/1 combination 2^n
+    X = np.asarray(list(itertools.product([0, 1], repeat=len(data[-1]))))
+    # Generating the traning y which contains only 1
+    # Notice here the same length with X
+    y = np.zeros(len(X))
+    print(y)
+
+    # For those features in the list X setting the result as 1
+    # This means these features are positive features
+    for item in data:
+        # Change the data to the ten-based
+        print(int(item, 2))
+        y[int(item, 2)] = 1
+    print(y)
+
+    # After getting the true class of y then setting the value 0 as -1
+    for index in range(len(y)):
+        if y[index] == 0:
+            y[index] = -1
+    print(y)
+
+
+score1 = train_perceptron(data1)
